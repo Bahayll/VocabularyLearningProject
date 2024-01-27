5d4c5f9645159cde4a7277b3616d4b8bddcb731d
Bahayll <bahayll221@gmail.com>
2024-01-27 13:59:03 +0300

    Completed Project

---
+++ LearnVocabulary/LearnVocabulary/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace LearnVocabulary
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtEnglish = new System.Windows.Forms.TextBox();
+            this.TxtTurkish = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LblTime = new System.Windows.Forms.Label();
+            this.LblWord = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.LblAnswer = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Snow;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(53, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "English :";
+            // 
+            // TxtEnglish
+            // 
+            this.TxtEnglish.Location = new System.Drawing.Point(127, 173);
+            this.TxtEnglish.Name = "TxtEnglish";
+            this.TxtEnglish.Size = new System.Drawing.Size(152, 26);
+            this.TxtEnglish.TabIndex = 10;
+            // 
+            // TxtTurkish
+            // 
+            this.TxtTurkish.Location = new System.Drawing.Point(127, 225);
+            this.TxtTurkish.Name = "TxtTurkish";
+            this.TxtTurkish.Size = new System.Drawing.Size(152, 26);
+            this.TxtTurkish.TabIndex = 1;
+            this.TxtTurkish.TextChanged += new System.EventHandler(this.TxtTurkish_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Turkish :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(348, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Time :";
+            // 
+            // LblTime
+            // 
+            this.LblTime.AutoSize = true;
+            this.LblTime.Location = new System.Drawing.Point(417, 176);
+            this.LblTime.Name = "LblTime";
+            this.LblTime.Size = new System.Drawing.Size(27, 18);
+            this.LblTime.TabIndex = 5;
+            this.LblTime.Text = "90";
+            this.LblTime.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // LblWord
+            // 
+            this.LblWord.AutoSize = true;
+            this.LblWord.Location = new System.Drawing.Point(417, 228);
+            this.LblWord.Name = "LblWord";
+            this.LblWord.Size = new System.Drawing.Size(18, 18);
+            this.LblWord.TabIndex = 7;
+            this.LblWord.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(346, 228);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 18);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Word :";
+            // 
+            // LblAnswer
+            // 
+            this.LblAnswer.AutoSize = true;
+            this.LblAnswer.Location = new System.Drawing.Point(124, 322);
+            this.LblAnswer.Name = "LblAnswer";
+            this.LblAnswer.Size = new System.Drawing.Size(172, 18);
+            this.LblAnswer.TabIndex = 8;
+            this.LblAnswer.Text = "visible özelliği kullanıldı";
+            this.LblAnswer.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(154, 270);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 34);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Change";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(661, 519);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.LblAnswer);
+            this.Controls.Add(this.LblWord);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.LblTime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtTurkish);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtEnglish);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "LearnEnglish";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtEnglish;
+        private System.Windows.Forms.TextBox TxtTurkish;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label LblTime;
+        private System.Windows.Forms.Label LblWord;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label LblAnswer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ LearnVocabulary/LearnVocabulary/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace LearnVocabulary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        OleDbConnection baglanti = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=""C:\Users\Baha YOLAL\Desktop\C# Project7 İngilizce kelime öğrenme\dbSozluk.accdb"";Persist Security Info=True");
+        //Provider=Microsoft.ACE.OLEDB.12.0;Data Source="C:\Users\Baha YOLAL\Desktop\C# Project7 İngilizce kelime öğrenme\dbSozluk.accdb";Persist Security Info=True
+        
+        Random rast = new Random();
+        int süre = 90;
+        int kelime = 0;
+        void getir()
+        {
+
+            int sayi;
+            sayi = rast.Next(1, 2490);
+
+            baglanti.Open();
+            OleDbCommand komut = new OleDbCommand("Select * from sozluk where id = @p1", baglanti);
+            komut.Parameters.AddWithValue("@p1", sayi);
+            OleDbDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+
+                TxtEnglish.Text = dr[1].ToString();
+                LblAnswer.Text = dr[2].ToString();
+                LblAnswer.Text = LblAnswer.Text.ToLower();
+            }
+            baglanti.Close();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            getir();
+            TxtTurkish.Focus();
+            timer1.Start();
+        }
+
+        private void TxtTurkish_TextChanged(object sender, EventArgs e)
+        {
+            if(TxtTurkish.Text==LblAnswer.Text)
+            {
+                kelime++;
+                LblWord.Text = kelime.ToString();
+                getir();
+                TxtTurkish.Clear();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            süre--;
+            LblTime.Text = süre.ToString();
+            if(süre == 0)
+            {
+                TxtTurkish.Enabled=false;
+                TxtEnglish.Enabled=false;
+                timer1.Stop();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            getir();
+            TxtTurkish.Clear();
+
+        }
+    }
+}
